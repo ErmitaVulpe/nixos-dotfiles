eb9de0981c6e15f4045689664e25a59436e3c4f6
Tymon Wilczek <tymon.wilczek@gmail.com>
2025-01-20 17:59:56 +0100

    Added firefox

--- configs/uwuntu/default.nix
+++ configs/uwuntu/default.nix
@@ -12,6 +12,7 @@
   users.users.root.initialHashedPassword = "$6$aS.0EG/z$7cgSogPyLF2IXtZmH7gn5CZaAWTDS3y71j1gnVh2m4MOgU9.AWtLmAjZIpn2TWcYuuM9HtJta/V3hg4xkPyT01";
   users.users.uwuntu.homeImports = [
     ../../home/bullshit
+    ../../home/firefox
     ../../home/foot
     ../../home/hyprland
   ];
