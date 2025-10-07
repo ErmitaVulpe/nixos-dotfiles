dfd09c3c970f724f6576c9b1f3ecb0242c1f9d7e
ErmitaVulpe <tymon.wilczek@gmail.com>
2025-10-07 18:29:09 +0200

    Actually added myself to the network group

--- configs/uwuntu/default.nix
+++ configs/uwuntu/default.nix
@@ -140,6 +140,11 @@
   users.mutableUsers = false;
   security.sudo.wheelNeedsPassword = false;
 
+  # System groups
+  users.groups = {
+    network = {};
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
