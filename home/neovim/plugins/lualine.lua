9220ae743877576ab78df6e575609612d02b4f01
ErmitaVulpe <tymon.wilczek@gmail.com>
2025-11-13 14:39:53 +0100

    nvim: Changed lualine path display

--- home/neovim/plugins/lualine.lua
+++ home/neovim/plugins/lualine.lua
@@ -1,13 +1,37 @@
 opts = {
-    icons_enabled = true
+    options = {
+        icons_enabled = true,
+    },
+    sections = {
+        lualine_c = {
+            {
+                'filename',
+                file_status = true,      -- Displays file status (readonly status, modified status)
+                newfile_status = false,  -- Display new file status (new file means no write after created)
+                path = 4,
+                -- 0: Just the filename
+                -- 1: Relative path
+                -- 2: Absolute path
+                -- 3: Absolute path, with tilde as the home directory
+                -- 4: Filename and parent dir, with tilde as the home directory
+
+                shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+                -- for other components. (terrible name, any suggestions?)
+                symbols = {
+                    modified = '[+]',      -- Text to show when the file is modified.
+                    readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+                    unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                    newfile = '[New]',     -- Text to show for newly created file before first write
+                },
+            },
+        },
+    },
 }
 
 if not (vim.fn.has("gui_running") == 1 or vim.env.COLORTERM or vim.env.TERM_PROGRAM) then
     opts.icons_enabled = false
-    -- opts.component_separators = { left = "|", right = "|" }
     opts.section_separators = { left = "", right = "" }
 end
 
-require("lualine").setup({
-    options = opts
-})
+require("lualine").setup(opts)
+
