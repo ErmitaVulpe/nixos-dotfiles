0cdc18bf6957dc6e9c809f041456e3923440b0a9
ErmitaVulpe <emily.wilczek@proton.me>
2026-05-01 11:39:07 +0200

    School: made questa licensing work

--- home/school/default.nix
+++ home/school/default.nix
@@ -10,12 +10,19 @@
   };
 
   config = lib.mkIf config.homeModules.school.enable {
-    home.packages = with pkgs; [
-      anki-bin
-      drawio
-      (quartus-prime-lite.overrideAttrs {
-        supportedDevices = [ "Cyclone IV" ];
-      })
-    ];
+    home = {
+      packages = with pkgs; [
+        anki-bin
+        drawio
+        (pkgs.quartus-prime-lite.overrideAttrs {
+          supportedDevices = [ "Cyclone IV" ];
+        })
+      ];
+
+      sessionVariables = {
+        SALT_LICENSE_SERVER = "${config.home.homeDirectory}/.altera.quartus/LR-163727_License.dat";
+        LM_LICENSE_FILE = "${config.home.homeDirectory}/.altera.quartus/LR-163727_License.dat";
+      };
+    };
   };
 }
