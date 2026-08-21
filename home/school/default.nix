3e72074054a7a6ff54c862eb930cab7c3d6cdbae
ErmitaVulpe <emily.wilczek@proton.me>
2026-08-21 10:08:16 +0200

    Removed unnecessary school apps

--- home/school/default.nix
+++ home/school/default.nix
@@ -12,17 +12,17 @@
   config = lib.mkIf config.homeModules.school.enable {
     home = {
       packages = with pkgs; [
-        anki-bin
+        # anki-bin
         drawio
-        (pkgs.quartus-prime-lite.overrideAttrs {
-          supportedDevices = [ "Cyclone IV" ];
-        })
+        # (pkgs.quartus-prime-lite.overrideAttrs {
+        #   supportedDevices = [ "Cyclone IV" ];
+        # })
       ];
 
-      sessionVariables = {
-        SALT_LICENSE_SERVER = "${config.home.homeDirectory}/.altera.quartus/LR-163727_License.dat";
-        LM_LICENSE_FILE = "${config.home.homeDirectory}/.altera.quartus/LR-163727_License.dat";
-      };
+      # sessionVariables = {
+      #   SALT_LICENSE_SERVER = "${config.home.homeDirectory}/.altera.quartus/LR-163727_License.dat";
+      #   LM_LICENSE_FILE = "${config.home.homeDirectory}/.altera.quartus/LR-163727_License.dat";
+      # };
     };
   };
 }
